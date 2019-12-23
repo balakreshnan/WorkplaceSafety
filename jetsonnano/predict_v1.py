ee00583ca6d7b5571ad56eee93e1ee2d2e357c42
bala_b@hotmail.com <bala_b@hotmail.com>
2019-12-22 19:15:57 -0600

    nano tensorflow

---
+++ jetsonnano/predict_v1.py
@@ -0,0 +1,57 @@
+# The steps implemented in the object detection sample code: 
+# 1. for an image of width and height being (w, h) pixels, resize image to (w', h'), where w/h = w'/h' and w' x h' = 262144
+# 2. resize network input size to (w', h')
+# 3. pass the image to network and do inference
+# (4. if inference speed is too slow for you, try to make w' x h' smaller, which is defined with DEFAULT_INPUT_SIZE (in object_detection.py or ObjectDetection.cs))
+"""Sample prediction script for TensorFlow 1.x."""
+import sys
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+from object_detection import ObjectDetection
+
+MODEL_FILENAME = 'model.pb'
+LABELS_FILENAME = 'labels.txt'
+
+
+class TFObjectDetection(ObjectDetection):
+    """Object Detection class for TensorFlow"""
+
+    def __init__(self, graph_def, labels):
+        super(TFObjectDetection, self).__init__(labels)
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            input_data = tf.placeholder(tf.float32, [1, None, None, 3], name='Placeholder')
+            tf.import_graph_def(graph_def, input_map={"Placeholder:0": input_data}, name="")
+
+    def predict(self, preprocessed_image):
+        inputs = np.array(preprocessed_image, dtype=np.float)[:, :, (2, 1, 0)]  # RGB -> BGR
+
+        with tf.Session(graph=self.graph) as sess:
+            output_tensor = sess.graph.get_tensor_by_name('model_outputs:0')
+            outputs = sess.run(output_tensor, {'Placeholder:0': inputs[np.newaxis, ...]})
+            return outputs[0]
+
+
+def main(image_filename):
+    # Load a TensorFlow model
+    graph_def = tf.GraphDef()
+    with tf.gfile.FastGFile(MODEL_FILENAME, 'rb') as f:
+        graph_def.ParseFromString(f.read())
+
+    # Load labels
+    with open(LABELS_FILENAME, 'r') as f:
+        labels = [l.strip() for l in f.readlines()]
+
+    od_model = TFObjectDetection(graph_def, labels)
+
+    image = Image.open(image_filename)
+    predictions = od_model.predict_image(image)
+    print(predictions)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print('USAGE: {} image_filename'.format(sys.argv[0]))
+    else:
+        main(sys.argv[1])
---
+++ jetsonnano/simple_camera.py
@@ -0,0 +1,110 @@
+# MIT License
+# Copyright (c) 2019 JetsonHacks
+# See license
+# Using a CSI camera (such as the Raspberry Pi Version 2) connected to a
+# NVIDIA Jetson Nano Developer Kit using OpenCV
+# Drivers for the camera and OpenCV are included in the base image
+
+import cv2
+
+# gstreamer_pipeline returns a GStreamer pipeline for capturing from the CSI camera
+# Defaults to 1280x720 @ 60fps
+# Flip the image by setting the flip_method (most common values: 0 and 2)
+# display_width and display_height determine the size of the window on the screen
+
+import sys
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+from object_detection import ObjectDetection
+
+MODEL_FILENAME = 'model.pb'
+LABELS_FILENAME = 'labels.txt'
+
+class TFObjectDetection(ObjectDetection):
+    """Object Detection class for TensorFlow"""
+
+    def __init__(self, graph_def, labels):
+        super(TFObjectDetection, self).__init__(labels)
+        self.graph = tf.compat.v1.Graph()
+        with self.graph.as_default():
+            input_data = tf.compat.v1.placeholder(tf.float32, [1, None, None, 3], name='Placeholder')
+            tf.import_graph_def(graph_def, input_map={"Placeholder:0": input_data}, name="")
+
+    def predict(self, preprocessed_image):
+        inputs = np.array(preprocessed_image, dtype=np.float)[:, :, (2, 1, 0)]  # RGB -> BGR
+
+        with tf.compat.v1.Session(graph=self.graph) as sess:
+            output_tensor = sess.graph.get_tensor_by_name('model_outputs:0')
+            outputs = sess.run(output_tensor, {'Placeholder:0': inputs[np.newaxis, ...]})
+            return outputs[0]
+
+
+def gstreamer_pipeline(
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+
+def show_camera():
+    # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
+    print(gstreamer_pipeline(flip_method=0))
+    cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+
+    # Load a TensorFlow model
+    graph_def = tf.compat.v1.GraphDef()
+    with tf.io.gfile.GFile(MODEL_FILENAME, 'rb') as f:
+        graph_def.ParseFromString(f.read())
+
+    # Load labels
+    with open(LABELS_FILENAME, 'r') as f:
+        labels = [l.strip() for l in f.readlines()]
+
+    od_model = TFObjectDetection(graph_def, labels)
+
+    if cap.isOpened():
+        window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
+        # Window
+        while cv2.getWindowProperty("CSI Camera", 0) >= 0:
+            ret_val, img = cap.read()
+            cv2.imshow("CSI Camera", img)
+            cv2.imwrite("image.jpg", img)
+
+            image = Image.open("image.jpg")
+            predictions = od_model.predict_image(image)
+            print(predictions)
+            # This also acts as
+            keyCode = cv2.waitKey(30) & 0xFF
+            # Stop the program on the ESC key
+            if keyCode == 27:
+                break
+        cap.release()
+        cv2.destroyAllWindows()
+    else:
+        print("Unable to open camera")
+
+
+if __name__ == "__main__":
+    show_camera()
